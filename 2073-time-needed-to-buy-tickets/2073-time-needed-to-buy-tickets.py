8b2041f220554e1e7c36cc37f4ecc3c5c802f5c5
Firomt <147161286+Firomt@users.noreply.github.com>
2024-01-03 19:41:33 +0300

    Time: 45 ms (75.11%), Space: 17.3 MB (11.13%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,15 +1,18 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        t=0
-        tick_k=tickets[k]
-        for i in range(len(tickets)):
-            if k<i and tickets[i]>= tick_k:
-                t+=tick_k-1
-            elif tickets[i] < tick_k:
-                t+=tickets[i]
-            else:
-                t+=tick_k
-        return t
+        q=deque([i,t] for i,t in enumerate(tickets))
+        count=0
+        while True:
+            i,t=q.popleft()
+            count+=1
+            if i==k and t-1==0:
+                return count
+            if t>1:
+                q.append([i,t-1])
+
+
+        
+        
 
 
                 
