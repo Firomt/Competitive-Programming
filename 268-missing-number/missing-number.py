ba69cebd5c00e491a3133d87a5f9f8476cfb2551
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-08 22:09:13 +0300

    Time: 131 ms (27.84%) | Memory: 17.9 MB (64.74%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,19 +1,22 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        #cycle sort concept
+        #for the numbers from range [0,n] after sorting the correct index for each number is nums[i] while i is the number itself
+        n=len(nums) 
         i=0
-        while i < len(nums):
-            if nums[i]==len(nums):
+        while i < n:
+            if nums[i]==n:
                 i+=1
                 continue
-            correct=nums[i]
+            correct=nums[i] 
             if nums[i] !=  nums[correct]:
                 nums[i],nums[correct]=nums[correct],nums[i]
             else:
                 i+=1
-        for i in range(len(nums)):
+        for i in range(n):
             if nums[i]!=i:
-                return i
-        return len(nums)
+                return i  #after sorting the numbers if the number and the index are not equal that means the index is the missing number
+        return n #if after sorting the numbers and their respective index are equal that means n is the missing number 
             
     
 
