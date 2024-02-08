c97234ff2cf726f851084e15c6ba2abc17e2045f
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-08 23:12:03 +0300

    Time: 552 ms (20.87%) | Memory: 30.3 MB (93.71%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,10 +1,11 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         i=0
-        while i < len(nums):
-            correct=nums[i]-1
+        #sort it using cycle sort
+        while i < len(nums): 
+            correct=nums[i]-1 #for numbers in range[1,n] the correct postion index for the number at nums[i] is i=nums[i]-1
             if nums[i]!= nums[correct]:
                 nums[i],nums[correct]=nums[correct],nums[i]
             else:
                 i+=1
-        return nums[-1]
+        return nums[-1] #after putting the numbers at their correct index the last element will be the duplicate as its duplicate has already occupied its index 
