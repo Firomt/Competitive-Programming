fa5846b92c2accbbde6264e85a650f9422d1ff4f
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-09 11:55:43 +0300

    Time: 272 ms (58.36%) | Memory: 24.8 MB (76.41%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -9,10 +9,9 @@ class Solution:
                 if nums[i] != nums[correct]:
                     nums[i], nums[correct]=nums[correct], nums[i]
                 else:
-                    if nums[i] not in duplicates:
-                        duplicates.append(nums[i])
+                    duplicates.append(nums[i])
                     i+=1
             else:
                 i+=1
-        return duplicates
+        return set(duplicates)
         
