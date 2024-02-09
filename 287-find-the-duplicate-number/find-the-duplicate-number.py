a83863dbf6887e8147c1ae9c53a3628536cd44e1
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-09 10:14:12 +0300

    Time: 477 ms (59.31%) | Memory: 30.5 MB (83.83%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -6,6 +6,8 @@ class Solution:
             correct=nums[i]-1 #for numbers in range[1,n] the correct postion index for the number at nums[i] is i=nums[i]-1
             if nums[i]!= nums[correct]:
                 nums[i],nums[correct]=nums[correct],nums[i]
+            elif i!=nums[i]-1 and nums[correct]==nums[i]:
+                return nums[i]
             else:
                 i+=1
         return nums[-1] #after putting the numbers at their correct index the last element will be the duplicate as its duplicate has already occupied its index 
