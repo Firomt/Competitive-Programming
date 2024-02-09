4ac2a4f08561e8cc4fbf26c68a6ca1b76cd3239e
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-09 10:24:06 +0300

    Time: 486 ms (50.44%) | Memory: 30.4 MB (93.71%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -4,10 +4,10 @@ class Solution:
         #sort it using cycle sort
         while i < len(nums): 
             correct=nums[i]-1 #for numbers in range[1,n] the correct postion index for the number at nums[i] is i=nums[i]-1
-            if nums[i]!= nums[correct]:
+            if nums[i]!= nums[correct]: 
                 nums[i],nums[correct]=nums[correct],nums[i]
-            elif i!=nums[i]-1 and nums[correct]==nums[i]:
-                return nums[i]
+            elif i!=nums[i]-1 and nums[correct]==nums[i]:#if the element at i hasnt occupied its correct index and if there is another element which is equal to element at i which occupied the correct index then the element at i 
+                return nums[i]                           #cant occupyt its correct index as its duplicate has already occupied it so it is the duplicate so return that element
             else:
                 i+=1
         return nums[-1] #after putting the numbers at their correct index the last element will be the duplicate as its duplicate has already occupied its index 
