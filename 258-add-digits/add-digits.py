2b62758d8e04ad590b0b40d929393de6d2d5a404
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-14 11:22:07 +0300

    Time: 26 ms (97.81%) | Memory: 16.5 MB (73.62%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -1,12 +1,6 @@
 class Solution:
     def addDigits(self, num: int) -> int:
- # Base case: If the number is a single digit, return it.
         if num < 10:
-            return num 
-        
-        # Recursive case: Sum the digits of the number.
-        # Example: 38 --> 3 + 8 --> 11
+            return num
         num = num // 10 + num % 10 
-        
-        # Recursively call the function with the updated number.
         return self.addDigits(num)
