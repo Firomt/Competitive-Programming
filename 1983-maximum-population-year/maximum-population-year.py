a9b301eb4d1d4e3ae73a5fda45ad3f6f28b1838a
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-21 16:39:08 +0300

    Time: 49 ms (33.73%) | Memory: 16.6 MB (54.91%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.py
+++ 1983-maximum-population-year/maximum-population-year.py
@@ -15,31 +15,4 @@ class Solution:
                 maxx=max(cur_max, maxx)
                 year=i[0]
         return year
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # pop = [0] * 2051
-        # res = 0
-        # for l in logs:
-        #     pop[l[0]] += 1
-        #     pop[l[1]] -= 1
-        
-        # for i in range(1950, 2050):
-        #     pop[i] += pop[i-1]
-        #     if pop[i] > pop[res]:
-        #         res = i
-        
-        # return res
         
