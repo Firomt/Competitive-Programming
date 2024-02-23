713c9710721308c66ba1ebe3dbb6ed8daaced5c4
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-23 12:44:31 +0300

    Time: 2390 ms (12.12%) | Memory: 17.5 MB (66.12%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.py
@@ -0,0 +1,25 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.bookings=Counter()
+        
+
+    def book(self, start: int, end: int) -> bool:
+        self.bookings[start]+=1
+        self.bookings[end]-=1
+        count_book=0
+        for book in sorted(self.bookings):
+            count_book += self.bookings[book]
+            if count_book >= 3:
+                self.bookings[start]-=1
+                self.bookings[end]+=1
+                return False
+        return True
+
+
+        
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
