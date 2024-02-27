a69a79701d54984fe93733b9f1a68c99553d2e13
Firomt <147161286+Firomt@users.noreply.github.com>
2024-02-27 13:57:13 +0300

    Time: 809 ms (85.15%) | Memory: 50.4 MB (76.24%) - LeetSync

--- 2240-intervals-between-identical-elements/intervals-between-identical-elements.py
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.py
@@ -1,32 +1,18 @@
 class Solution:
     def getDistances(self, arr: List[int]) -> List[int]:
-        # formula -> suffix - (distance from right * index value) + prefix - (distance from left * index value)
-        d = defaultdict(list)
-        prefix = defaultdict(list)
-        suffix = defaultdict(list)
-        for i in range(len(arr)):
-            if arr[i] in d:
-                prefix[arr[i]].append(prefix[arr[i]][-1] + i)
-            else:
-                prefix[arr[i]].append(i)
-            d[arr[i]].append(i)
-
-        for i, val in d.items():
-            summ = 0
-            for idx in range(len(val)-1, -1, -1):
-                summ += val[idx]
-                suffix[i].append(summ)
-            suffix[i] = suffix[i][::-1]
-
-        ans = [0] * len(arr)
-        for i, val in suffix.items():
-            n = len(val)
-            for j in range(n):
-                summ = (abs(prefix[i][j] - ((j + 1) * d[i][j])) + abs(suffix[i][j] - ((n - j)* d[i][j])))
-                ans[d[i][j]] = summ
-        return ans
-
-
+        index_map=defaultdict(list)
+        res=[0]*len(arr)
+        for i,n in enumerate(arr):
+            index_map[n].append(i)
+        
+        for indices in index_map.values():
+            total=sum(indices)
+            size=len(indices)
+            leftsum=0
+            for i,ind in enumerate(indices):
+                res[ind]=i*ind-leftsum + (total-leftsum-ind)-(size-i-1)*ind
+                leftsum+=ind
+        return res
 
         
 
