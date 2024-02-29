b52d89309d150df0310b3a9250dd1b9fc0a7d987
Firomt <147161286+Firomt@users.noreply.github.com>
2024-03-01 00:11:19 +0300

    Time: 565 ms (76.22%) | Memory: 22 MB (72.73%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -1,35 +1,23 @@
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        def squareSum(r1, c1, r2, c2):
-            return preSum[r2][c2] - preSum[r1 - 1][c2] - preSum[r2][c1 - 1] + preSum[r1 - 1][c1 - 1]
+        rows,cols=len(mat),len(mat[0])
+        prefix_sum=[[0]*(cols+1) for _ in range(rows+1)]
 
-        m, n = len(mat), len(mat[0])
+        for i in range(rows):
+            for j in range(cols):
+                prefix_sum[i+1][j+1]=mat[i][j]+prefix_sum[i][j+1]+prefix_sum[i+1][j]-prefix_sum[i][j]
 
-        # preSum[i][j] := sum of all cells ending at mat[i][j] moving at right and down directions
-        preSum = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        def sumsquare(r1,c1,r2,c2):
+            return prefix_sum[r2+1][c2+1]-prefix_sum[r2+1][c1]-prefix_sum[r1][c2+1]+prefix_sum[r1][c1]
+        
 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                curr = mat[i - 1][j - 1]
-                diagonal = preSum[i - 1][j - 1]
-                above = preSum[i - 1][j]
-                left = preSum[i][j - 1]
-                # preSum[i][j] = curr + diagonal + (above - diagonal) + (left - diagonal)
-                #              = curr + above + left - diagonal
-                preSum[i][j] = curr + above + left - diagonal
+        best=0
+        for i in range(rows):
+            for j in range(cols):
+                for k in range(best, min(rows-i,cols-j)):
+                    if sumsquare(i,j,i+k,j+k) <= threshold:
+                        best=k+1
+                    else: 
+                        break
 
-        res = 0
-
-        # find square Sum starting at (i, j)
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                limit = min(m - i, n - j)
-                # build square with the size of l
-                for l in range(limit + 1):
-                    curr_sum = squareSum(i, j, i + l, j + l)
-
-                    # valid; update the maximum side-length
-                    if curr_sum <= threshold:
-                        res = max(res, l + 1)
-
-        return res
+        return best
