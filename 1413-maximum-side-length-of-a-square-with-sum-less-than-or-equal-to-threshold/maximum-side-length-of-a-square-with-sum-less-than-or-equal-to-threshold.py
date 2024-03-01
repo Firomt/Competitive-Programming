cea9b35f862411c4ee7cbdebdbb6cbca4646d21d
Firomt <147161286+Firomt@users.noreply.github.com>
2024-03-01 14:06:39 +0300

    Time: 539 ms (77.62%) | Memory: 22 MB (58.74%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -1,15 +1,20 @@
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        #Follow same approach as in for https://leetcode.com/problems/range-sum-query-2d-immutable/description/
+       
         rows,cols=len(mat),len(mat[0])
         prefix_sum=[[0]*(cols+1) for _ in range(rows+1)]
 
         for i in range(rows):
+            prefix=0
             for j in range(cols):
-                prefix_sum[i+1][j+1]=mat[i][j]+prefix_sum[i][j+1]+prefix_sum[i+1][j]-prefix_sum[i][j]
+                prefix+=mat[i][j]
+                above_sum=prefix_sum[i][j+1]
+                prefix_sum[i+1][j+1]=prefix+above_sum
 
         def sumsquare(r1,c1,r2,c2):
-            return prefix_sum[r2+1][c2+1]-prefix_sum[r2+1][c1]-prefix_sum[r1][c2+1]+prefix_sum[r1][c1]
-        
+            return prefix_sum[r2+1][c2+1]-prefix_sum[r1][c2+1]-prefix_sum[r2+1][c1]+prefix_sum[r1][c1]
+          
 
         best=0
         for i in range(rows):
