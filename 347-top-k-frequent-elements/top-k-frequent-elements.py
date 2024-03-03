080c8cd2dd46181e8a84acef384e0054242c8278
Firomt <147161286+Firomt@users.noreply.github.com>
2024-03-03 15:37:51 +0300

    Time: 141 ms (5.92%) | Memory: 21.1 MB (81.19%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,13 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         top_freq=[]
-        freq_hash=defaultdict(int)
-        for i in range(len(nums)):
-            if nums[i] in freq_hash:
-                freq_hash[nums[i]]+=1
-            else:
-                freq_hash[nums[i]]=1
-
+        freq_hash=Counter(nums)
+        
         while k>0:
             cand_key=max(freq_hash, key=freq_hash.get)
             top_freq.append(cand_key)
