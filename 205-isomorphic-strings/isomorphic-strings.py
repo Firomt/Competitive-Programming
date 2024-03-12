b70c6df99976f60882c382148bc45f05c687d990
Firomt <147161286+Firomt@users.noreply.github.com>
2024-03-12 17:08:44 +0300

    Time: 34 ms (93.51%) | Memory: 16.7 MB (68.98%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        sMap,tMap={},{}
+        for i in range(len(s)):
+            c1,c2= s[i],t[i]
+            if((c1 in sMap and sMap[c1]!= c2) or (c2 in tMap and tMap[c2]!=c1)):
+                return False
+            sMap[c1]=c2
+            tMap[c2]=c1
+        return True
+        
+        
+
+        
+        
