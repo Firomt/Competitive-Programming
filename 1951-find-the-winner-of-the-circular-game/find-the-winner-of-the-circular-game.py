a8b8ae247672d460e5352cb855c11a339810742f
Firomt <147161286+Firomt@users.noreply.github.com>
2024-03-13 21:08:50 +0300

    Time: 151 ms (17.71%) | Memory: 16.7 MB (43.87%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -4,9 +4,9 @@ class Solution:
         for i in range(1,n+1):
             queue.append(i)   
         while len(queue) > 1:
-            for i in range(0,k):
+            for i in range(0,k):  #shift the array k times
                 queue.append(queue[0])
                 queue.popleft()
-            queue.pop()   
-        return queue[0]
+            queue.pop() # the element we get at exactly k'th shift is the one to be removed and this elemnt will be at the end of the queue after shifting it so we pop this element  
+        return queue[0]  #after popping all elements one element will be left and that is the winner
         
