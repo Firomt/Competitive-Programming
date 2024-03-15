6cf7ba6eef47410dd1e5ee7f4649e1966b5a479e
Firomt <147161286+Firomt@users.noreply.github.com>
2024-03-15 16:52:04 +0300

    Time: 50 ms (58.44%) | Memory: 16.8 MB (94.33%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                nums[i]=nums[i]*2
+                nums[i+1]=0
+        n=len(nums)
+        i = 0
+        for j in range(n):
+            if (nums[j] != 0):
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+
+        return nums
+
+        
