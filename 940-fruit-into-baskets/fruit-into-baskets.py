0bcd4177295927769a9e251ee9ced0ea97b4cd4a
Firomt <147161286+Firomt@users.noreply.github.com>
2024-04-06 12:44:50 +0300

    Time: 649 ms (79.01%) | Memory: 24.2 MB (23.07%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,22 +1,21 @@
+from typing import List
+
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        res = 0
+        l, r, max_frt = 0, 0, 0
         basket = {}
-        total = 0
-        l =0
-        for r in range(len(fruits)):
-            basket[fruits[r]] = 1 + basket.get(fruits[r], 0)
-            total += 1
+        while r < len(fruits):
+            if len(basket) < 2 or fruits[r] in basket:
+                basket[fruits[r]] = 1 + basket.get(fruits[r], 0)
+            else:
+                max_frt = max(max_frt, r - l)
+                while len(basket) >= 2:
+                    removed = fruits[l]
+                    basket[removed] -= 1
+                    if basket[removed] == 0:
+                        del basket[removed]
+                    l += 1
+                basket[fruits[r]] = 1
+            r += 1
+        return max(max_frt, r - l)
 
-            while len(basket) > 2:
-                f = fruits[l]
-                basket[f] -= 1
-                total -= 1
-                l += 1
-                if basket[f]==0:
-                    basket.pop(f)
-            res = max(res, total)
-        return res
-            
-                
-        
