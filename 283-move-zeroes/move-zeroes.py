1b8be33229152b1c5081a48f9ff903dec4a64587
Firomt <147161286+Firomt@users.noreply.github.com>
2024-04-07 13:15:08 +0300

    Time: 122 ms (79.64%) | Memory: 18.1 MB (31.29%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+     def moveZeroes(self, nums):
+        n = len(nums)
+        i = 0
+        for j in range(n):
+            if (nums[j] != 0):
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        
