dc95cf647fb169d18b6de9fcac69153f77a60fb6
Firomt <147161286+Firomt@users.noreply.github.com>
2024-04-08 09:51:44 +0300

    Time: 923 ms (87.33%) | Memory: 32.3 MB (91.17%) - LeetSync

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
@@ -1,9 +1,9 @@
 class Solution:
     def rearrangeArray(self, nums):
-        n = len(nums)
-        for i in range(1, n - 1):
-            if (nums[i - 1] < nums[i] < nums[i + 1]) or (nums[i - 1] > nums[i] > nums[i + 1]):
-                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        for i in range(1,len(nums)-1):
+            if (nums[i-1]<nums[i] < nums[i+1]) or (nums[i-1]>nums[i]>nums[i+1]):
+                nums[i+1],nums[i]=nums[i],nums[i+1]
         return nums
+
 
         
