3cf9034313540297c9814bb7250e414119903a76
Firomt <147161286+Firomt@users.noreply.github.com>
2024-04-21 21:56:10 +0300

    Time: 393 ms (79.35%) | Memory: 29.4 MB (69.27%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,21 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        n=len(skill)
+        skill.sort()
+        i, j=1, n-2
+        chem=skill[0]*skill[n-1]
+        tot_skill=skill[0]+skill[n-1]
+        if n==2:
+            return skill[0]*skill[1]
+        while i<j:
+            if skill[i]+ skill[j]==tot_skill:
+                chem += skill[i] * skill[j]
+            else:
+                return -1
+            i+=1
+            j-=1
+        return chem
+
+        
+
+        
