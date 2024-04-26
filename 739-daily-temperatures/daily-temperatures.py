dfa26d087ebb4cddd84f2b51c544ae4c370cefc4
Firomt <147161286+Firomt@users.noreply.github.com>
2024-04-26 17:28:40 +0300

    Time: 910 ms (39.08%) | Memory: 32.1 MB (24.27%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,14 +1,19 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ans=[0]*len(temperatures)
         stack=[]
-        for i, t in enumerate(temperatures):
+        ans=[0]*len(temperatures)
+        for i,t in enumerate(temperatures):
             while stack and t > stack[-1][0]:
                 temp,Tindex=stack.pop()
                 ans[Tindex]=i-Tindex
             stack.append([t,i])
-        return ans
-    
+        return ans 
+
+
+
+
+
+       
 
             
       
