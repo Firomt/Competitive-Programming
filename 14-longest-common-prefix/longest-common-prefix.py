43db73959a3e3761f15f810835e4692ad5c47321
Firomt <147161286+Firomt@users.noreply.github.com>
2024-05-29 20:30:43 +0300

    Time: 34 ms (80.06%) | Memory: 16.5 MB (87.94%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if len(strs) == 0:
+            return ""
+
+        base = strs[0]
+        for i in range(len(base)):
+            for word in strs[1:]:
+                if i == len(word) or word[i] != base[i]:
+                    return base[0:i]
+
+        return base
+        
